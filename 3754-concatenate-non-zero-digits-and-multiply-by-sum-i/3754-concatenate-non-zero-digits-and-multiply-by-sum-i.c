92fea91ac0c9bd77df297714427502052638a2d6
Vinayak Shankar <vinayakshankar33@gmail.com>
2026-07-07 12:56:16 +0530

    Time: 1 ms (4.62%), Space: 9.4 MB (23.08%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.c
@@ -0,0 +1,23 @@
+long long sumAndMultiply(int n) {
+    if (n == 0) return 0;
+
+    int digits[10];
+    int len = 0;
+
+    while (n > 0) {
+        digits[len++] = n % 10;
+        n /= 10;
+    }
+
+    long long x = 0;
+    int sum = 0;
+
+    for (int i = len - 1; i >= 0; i--) {
+        if (digits[i] != 0) {
+            x = x * 10 + digits[i];
+            sum += digits[i];
+        }
+    }
+
+    return x * sum;
+}
